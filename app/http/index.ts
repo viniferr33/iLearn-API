89ec59a7de475bc76ef98c35a2bb4cc5436ae021
Vinicius Ferreira Gomes <vini.ferr33@gmail.com>
2023-04-29 14:45:43 -0300

    E1I-50 / added CreateStudent Adapter and added it to routing server

--- app/http/index.ts
+++ app/http/index.ts
@@ -1,7 +1,9 @@
 import ExpressServer from "./Server";
+import CreateStudentAdapter from "./adapters/Student/CreateStudent.adapter";
 import { PORT } from "./config";
 
 const myServer = new ExpressServer(PORT);
+myServer.addRoute(CreateStudentAdapter);
 
 myServer.setup();
 myServer.start();
