9dcac8b26de0adf699349a1f3385f2148ca2c3b9
Vinicius Ferreira Gomes <vini.ferr33@gmail.com>
2023-05-20 00:00:38 -0300

    fix: login and swagger

--- app/http/config.ts
+++ app/http/config.ts
@@ -1,6 +1,38 @@
 import PostgreService from "../../src/infra/db.SQL/PostgreService";
 import SQLServerService from "../../src/infra/db.SQL/SQLServerService";
 
+const UNGERISTERED_PATHS = [
+  {
+    path: "/api/docs",
+    method: "GET",
+  },
+  {
+    path: "/api/docs/swagger-ui-bundle.js",
+    method: "GET",
+  },
+  {
+    path: "/api/docs/swagger-ui-init.js",
+    method: "GET",
+  },
+  {
+    path: "/api/docs/swagger-ui-standalone-preset.js",
+    method: "GET",
+  },
+  {
+    path: "/api/docs/swagger-ui.css",
+    method: "GET",
+  },
+  /////////////////////////////////////////////
+  {
+    path: "/api/students",
+    method: "POST",
+  },
+  {
+    path: "/api/login",
+    method: "POST",
+  },
+];
+
 const PORT = 8080;
 
 const POSTGRE = new PostgreService({
@@ -18,4 +50,4 @@ const SQL_SERVER = new SQLServerService({
   user: process.env.DB_USER ?? "",
 });
 
-export { POSTGRE, SQL_SERVER, PORT };
+export { POSTGRE, SQL_SERVER, PORT, UNGERISTERED_PATHS };
--- app/http/handlers/AuthHandler.ts
+++ app/http/handlers/AuthHandler.ts
@@ -4,6 +4,7 @@ import ValidateAccessToken from "../../../src/core/useCases/Student/validateAcce
 import RepositoryFactory from "../../../src/gateways/repositories";
 import LoginInput from "../../../src/dtos/Login.input";
 import GenerateAccessTokenUseCase from "../../../src/core/useCases/Student/generateAccessToken.usecase";
+import { UNGERISTERED_PATHS } from "../config";
 
 export default class AuthHandler {
   private validateAccessToken: ValidateAccessToken;
@@ -16,17 +17,26 @@ export default class AuthHandler {
     this.generateAcessToken = new GenerateAccessTokenUseCase(repositoryFactory);
   }
 
+  private checkUnregisteredPaths(path: string, method: string): boolean {
+    const foundPath = UNGERISTERED_PATHS.find(
+      (element) => element.path === path
+    );
+    return foundPath !== undefined && foundPath.method === method;
+  }
+
   public async handle(
     request: Request,
     response: Response,
     next: NextFunction
   ): Promise<void> {
     try {
-      if (request.baseUrl.endsWith("login")) {
-        console.log("Aqui");
-        const data = request.body as LoginInput;
-        const accessToken = await this.generateAcessToken.execute(data);
-        response.json(accessToken);
+      const { baseUrl, method } = request;
+      if (this.checkUnregisteredPaths(baseUrl, method)) {
+        if (request.baseUrl.endsWith("login")) {
+          const data = request.body as LoginInput;
+          const accessToken = await this.generateAcessToken.execute(data);
+          response.json(accessToken);
+        } else next();
       } else {
         const data = request.query as LoginInput;
         await this.validateAccessToken.execute(data);
